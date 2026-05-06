ea9c43323e2014eb04b71086c976136b921eb948
GauravSB07 <gauravsb747@gmail.com>
2026-05-06 19:51:33 +0530

    Fixed project module name

--- NutriCore/wsgi.py
+++ NutriCore/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYM_APP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NutriCore.settings')
 
 application = get_wsgi_application()
