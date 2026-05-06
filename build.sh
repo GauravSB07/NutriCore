19df0f60edbb450f2feefecbf5bfe4d645ac8935
GauravSB07 <gauravsb747@gmail.com>
2026-05-06 22:15:36 +0530

    Added Superuser Creation

--- build.sh
+++ build.sh
@@ -8,13 +8,24 @@ pip install -r requirements.txt
 echo "Running migrations..."
 python manage.py migrate --verbosity 3
 
-echo "Checking files..."
-ls -la
-
 echo "Loading data..."
 python manage.py loaddata data.json --verbosity 3
 
+echo "Creating superuser..."
+python manage.py shell << END
+from django.contrib.auth.models import User
+import os
+
+username = os.environ.get("ADMIN_USERNAME")
+email = os.environ.get("ADMIN_EMAIL")
+password = os.environ.get("ADMIN_PASSWORD")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+END
+
 echo "Collecting static..."
 python manage.py collectstatic --noinput
-
-echo "BUILD FINISHED"
